82ad0ed73adc3b72f3c2790596c8d3127a1561e2
jleow <jleow@iupui.edu>
2017-06-07 14:09:55 -0400

    Added a line to append to an array

--- app.js
+++ app.js
@@ -4,12 +4,13 @@
  */
 
 const app = {
-  init(formSelector) {    //Note: This is the same as init: function() {}
+  init(selectors) {    //Note: This is the same as init: function() {}
+    this.flicks = []
     this.max = 0  //Keep track of the property of id. Will be the same as saying max: 0. This is another property we're assigning to the Obj app.
+    this.list = document.querySelector(selectors.listSelector)
     document
-      .querySelector(formSelector)
+      .querySelector(selectors.formSelector)
       .addEventListener('submit', this.addFlick.bind(this))  //This will pass in whatever formSelector that is passed through the caller app.init()
-
   },      //**DONT FORGET THE COMMA!
 
   addFlick(ev) {
@@ -21,9 +22,22 @@ const app = {
       name: f.flickName.value,
     }
 
-    console.log(flick.name, flick.id)
+    const listItem = this.renderListItem(flick)
+    this.list.appendChild(listItem)
+    this.flicks.push(listItem)
+
     ++ this.max
+  },
+
+  renderListItem(flick) {
+    const item = document.createElement('li')
+    item.textContent = flick.name
+    return item
   },
 }
 
-app.init('#flickForm')
+app.init({
+  formSelector: '#flick-form',
+  listSelector: '#flick-list',
+
+})
